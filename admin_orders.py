c750792f5f8d9aa9f0d0455630fdeb08e0afdd34
LIMEN-mse <jajaslonia97@gmail.com>
2026-07-20 04:53:36 +0200

    Dodano moduł zamówień

--- admin_orders.py
+++ admin_orders.py
@@ -1,3 +1,26 @@
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+
+from orders import change_status
 
 router = Router()
+
+
+@router.callback_query(F.data.startswith("accepted_"))
+async def accepted(callback: CallbackQuery):
+    await change_status(callback, "PRZYJĘTE", "🟢")
+
+
+@router.callback_query(F.data.startswith("progress_"))
+async def progress(callback: CallbackQuery):
+    await change_status(callback, "W REALIZACJI", "🚗")
+
+
+@router.callback_query(F.data.startswith("ready_"))
+async def ready(callback: CallbackQuery):
+    await change_status(callback, "GOTOWE", "📦")
+
+
+@router.callback_query(F.data.startswith("cancel_"))
+async def cancel(callback: CallbackQuery):
+    await change_status(callback, "ODRZUCONE", "❌")
